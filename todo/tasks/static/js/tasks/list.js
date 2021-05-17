f4de3479c30fd9d4ffe89dddf65cc15c737ebf7a
VishwasGajawada <vishwasgajawada2002@gmail.com>
2021-05-17 15:52:33 +0530

    separated static files

---
+++ todo/tasks/static/js/tasks/list.js
@@ -0,0 +1,26 @@
+// var editButtons = document.querySelectorAll('.edit-button');
+// var submitButtons = document.querySelectorAll('.submit-button');
+// function editClose(e){
+//     var parent = e.parentElement;
+//     console.log(parent);
+// }
+// function editOpen(e){
+    
+// }
+// function changeEditStatus(e){
+//     console.log("Opening "+e.id);
+//     if(e.innerHTML=="Edit"){
+//         editOpen(e);
+//         console.log("Opening "+e.id);
+//     }else{
+//         editClose(e);
+//     }
+// }
+// for (const editButton of editButtons){
+//     editButton.addEventListener('click',()=>changeEditStatus(editButton));
+// }
+// for (const editButton of editButtons){
+//     editClose(editButton);
+// }
+// console.log(editButtons);
+console.log("hi");
