6e50f0bc12d4b5bac261264f0c896f8ea240909f
VishwasGajawada <vishwasgajawada2002@gmail.com>
2021-05-26 10:28:35 +0530

    formatting

--- quizzo/quizapp/forms.py
+++ quizzo/quizapp/forms.py
@@ -3,43 +3,76 @@ from django.contrib.auth.forms import UserCreationForm
 from django.forms import fields
 from django.template.defaultfilters import slugify
 from django.core.exceptions import ValidationError
-from quizapp.models import (MyUser,Student,Teacher,Subject,Quiz,Answer)
+from .models import MyUser, Student, Teacher, Subject, Quiz, Answer
+
 
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = MyUser
-        fields = ('username','email','password1','password2','profile_pic')
+        fields = ("username", "email", "password1", "password2", "profile_pic")
+
 
 class StudentCreationForm(forms.ModelForm):
-    subjects = forms.ModelMultipleChoiceField(label = 'Subjects',queryset = Subject.objects.all(),widget=forms.CheckboxSelectMultiple,)
+    subjects = forms.ModelMultipleChoiceField(
+        label="Subjects",
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Student
-        fields = ['subjects','registration_number','roll_no']
-        help_texts = {'subjects' : 'Select subjects you are enrolled or interested in'} 
-    
+        fields = ["subjects", "registration_number", "roll_no"]
+        help_texts = {"subjects": "Select subjects you are enrolled or interested in"}
+
+
 class TeacherCreationForm(forms.ModelForm):
-    subjects = forms.ModelMultipleChoiceField(label = 'Subjects',queryset = Subject.objects.all(),widget=forms.CheckboxSelectMultiple,)
+    subjects = forms.ModelMultipleChoiceField(
+        label="Subjects",
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Teacher
-        fields = ['subjects']
-        help_texts = {'subjects' : 'Select subjects you assigned to'}
+        fields = ["subjects"]
+        help_texts = {"subjects": "Select subjects you assigned to"}
+
 
 class SubjectForm(forms.ModelForm):
     class Meta:
         model = Subject
-        fields = ['name','color']
+        fields = ["name", "color"]
+
 
 class StudentSubjectUpdateForm(forms.ModelForm):
-    subjects = forms.ModelMultipleChoiceField(label = 'Subjects',queryset = Subject.objects.all(),widget=forms.CheckboxSelectMultiple,)
+    subjects = forms.ModelMultipleChoiceField(
+        label="Subjects",
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Student
-        fields = ['subjects',]
+        fields = [
+            "subjects",
+        ]
+
+
 class TeacherSubjectUpdateForm(forms.ModelForm):
-    subjects = forms.ModelMultipleChoiceField(label = 'Subjects',queryset = Subject.objects.all(),widget=forms.CheckboxSelectMultiple,)
+    subjects = forms.ModelMultipleChoiceField(
+        label="Subjects",
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Teacher
 
-        fields = ['subjects',]
+        fields = [
+            "subjects",
+        ]
+
+
 class MyUserUpdateForm(forms.ModelForm):
     # readonly = ('username',)
     # def __init__(self, *arg, **kwrg):
@@ -56,12 +89,16 @@ class MyUserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = MyUser
-        fields = ['email','profile_pic']
+        fields = ["email", "profile_pic"]
 
 
 class StudentUpdateForm(forms.ModelForm):
     # readonly = ('registration_number',)
-    subjects = forms.ModelMultipleChoiceField(label = 'Subjects',queryset = Subject.objects.all(),widget=forms.CheckboxSelectMultiple,)
+    subjects = forms.ModelMultipleChoiceField(
+        label="Subjects",
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
 
     # def __init__(self, *arg, **kwrg):
     #     super(StudentUpdateForm, self).__init__(*arg, **kwrg)
@@ -77,20 +114,28 @@ class StudentUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Student
-        fields = ['roll_no','subjects']
+        fields = ["roll_no", "subjects"]
+
 
 class TeacherUpdateForm(forms.ModelForm):
-    subjects = forms.ModelMultipleChoiceField(label = 'Subjects',queryset = Subject.objects.all(),widget=forms.CheckboxSelectMultiple,)
+    subjects = forms.ModelMultipleChoiceField(
+        label="Subjects",
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Teacher
-        fields = ['subjects']
+        fields = ["subjects"]
+
 
 class QuizForm(forms.ModelForm):
     class Meta:
         model = Quiz
-        fields = ['title','description','question_file']
+        fields = ["title", "description", "question_file"]
+
 
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
-        fields = ['answer_file',]
+        fields = ["answer_file",]
--- quizzo/quizapp/models.py
+++ quizzo/quizapp/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.template.defaultfilters import slugify
 from PIL import Image
-from quizapp.utils import *
+from .utils import *
 
 # Create your models here.
 class MyUser(AbstractUser):
     email = models.EmailField(max_length=264)
-    profile_pic = models.ImageField(upload_to='profile_pics/',blank=True,null=True)
+    profile_pic = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
+
     def save(self, *args, **kwargs):
         super().save()
         if self.profile_pic:
@@ -23,60 +24,71 @@ class MyUser(AbstractUser):
 
 class Subject(models.Model):
     COLOR_CHOICES = [
-        ( "#E06C75" ,"RED" ),
-        ("#E5C07B" ,"YELLOW"),
-        ("#98C379" ,"GREEN"),
-        ("#61AFEF" , "BLUE"),
-        ("#863A95","PURPLE"),
+        ("#E06C75", "RED"),
+        ("#E5C07B", "YELLOW"),
+        ("#98C379", "GREEN"),
+        ("#61AFEF", "BLUE"),
+        ("#863A95", "PURPLE"),
     ]
-    name = models.CharField(max_length=256,primary_key=True)
-    color = models.CharField(max_length=20,choices=COLOR_CHOICES,default="#863A95")
+    name = models.CharField(max_length=256, primary_key=True)
+    color = models.CharField(max_length=20, choices=COLOR_CHOICES, default="#863A95")
     slug = models.SlugField(unique=True)
+
     def __str__(self):
         return self.name
 
-    def save(self,*args,**kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Subject,self).save(*args,**kwargs)
+        super(Subject, self).save(*args, **kwargs)
+
 
 class Student(models.Model):
-    user = models.OneToOneField(MyUser,on_delete = models.CASCADE,related_name='student')
-    subjects = models.ManyToManyField(Subject,related_name="students",blank=True)
+    user = models.OneToOneField(
+        MyUser, on_delete=models.CASCADE, related_name="student"
+    )
+    subjects = models.ManyToManyField(Subject, related_name="students", blank=True)
     registration_number = models.IntegerField(primary_key=True)
     roll_no = models.IntegerField()
 
     def __str__(self):
-        return self.user.username + " "+str(self.registration_number)
+        return self.user.username + " " + str(self.registration_number)
+
 
 class Teacher(models.Model):
-    user = models.OneToOneField(MyUser,on_delete = models.CASCADE,related_name='teacher')
-    subjects = models.ManyToManyField(Subject,related_name="teachers",blank=True)
+    user = models.OneToOneField(
+        MyUser, on_delete=models.CASCADE, related_name="teacher"
+    )
+    subjects = models.ManyToManyField(Subject, related_name="teachers", blank=True)
 
     def __str__(self):
         return self.user.username
 
+
 class Quiz(models.Model):
-    teacher = models.ForeignKey(Teacher,on_delete = models.CASCADE,related_name="quizzes")
-    subject = models.ForeignKey(Subject,on_delete=models.CASCADE,related_name="quizzes")
+    teacher = models.ForeignKey(
+        Teacher, on_delete=models.CASCADE, related_name="quizzes"
+    )
+    subject = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, related_name="quizzes"
+    )
     title = models.CharField(max_length=256)
-    description = models.CharField(max_length=300,blank=True,null=True)
+    description = models.CharField(max_length=300, blank=True, null=True)
     created_time = models.DateTimeField(auto_now_add=True)
-    question_file = models.FileField(upload_to='question_files/',blank=True,null=True)
-    avg_score = models.FloatField(default=None,blank=True,null=True)
+    question_file = models.FileField(upload_to="question_files/", blank=True, null=True)
+    avg_score = models.FloatField(default=None, blank=True, null=True)
 
     def __str__(self):
-        return self.subject.name+"-"+self.title+" - "+str(self.id)
+        return self.subject.name + "-" + self.title + " - " + str(self.id)
 
 
 class Answer(models.Model):
-    student = models.ForeignKey(Student,on_delete = models.CASCADE,related_name='answers')
-    quiz = models.ForeignKey(Quiz,on_delete = models.CASCADE,related_name="answers")
-    score = models.PositiveIntegerField(blank=True,null=True)
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="answers"
+    )
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="answers")
+    score = models.PositiveIntegerField(blank=True, null=True)
     created_time = models.DateTimeField(auto_now_add=True)
-    answer_file = models.FileField(upload_to='answer_files/')
+    answer_file = models.FileField(upload_to="answer_files/")
 
     def __str__(self):
-        return self.quiz.title+"-answer-"+str(self.id)
-
-
-
+        return self.quiz.title + "-answer-" + str(self.id)
