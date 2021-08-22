8d283b298507dfa64543f614ec9ea4e918455d05
VishwasGajawada <vishwasgajawada2002@gmail.com>
2021-08-22 13:26:20 +0530

    add-blog and add-comments functionality

--- blog-comments-test/blog_app/urls.py
+++ blog-comments-test/blog_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('blog_detail/<str:pk>/',views.blog_detail,name="blog_detail"),
+    path('add_blog', views.add_blog, name="add_blog"),
+    path('blog_detail/<str:pid>/add_reply/<str:cid>', views.add_reply, name="add_reply")
 ]
--- blog-comments-test/blog_app/views.py
+++ blog-comments-test/blog_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog, Comment
+from .forms import BlogForm, CommentForm
 
 # Create your views here.
 def index(request):
@@ -7,9 +8,48 @@ def index(request):
     context = {'blogs' : blogs}
     return render(request, 'blog_app/index.html',context=context)
 
+def add_blog(request):
+    form = BlogForm()
+
+    if request.method == 'POST':
+        form  = BlogForm(request.POST)
+        if form.is_valid():
+            blog = form.save()
+            return redirect('/')
+
+    context = {'form' : form}
+    return render(request, 'blog_app/add_blog.html', context=context)
+
 def blog_detail(request, pk):
     blog = Blog.objects.get(id=pk)
     comments = Comment.objects.filter(post = blog).order_by('path_id')
+    comment_form = CommentForm()
 
-    context={'blog': blog, 'comments': comments}
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = blog
+            comment.save()
+            return redirect('blog_detail',pk=pk)
+
+    context={'blog': blog, 'comments': comments, 'comment_form':comment_form}
     return render(request, 'blog_app/blog_detail.html',context=context)
+
+def add_reply(request,pid,cid):
+    blog = Blog.objects.get(id = pid)
+    parent_comment = Comment.objects.get(id = cid)
+
+    reply_form = CommentForm()
+
+    if request.method == 'POST':
+        reply_form = CommentForm(request.POST)
+        if reply_form.is_valid():
+            reply = reply_form.save(commit=False)
+            reply.post = blog
+            reply.parent_comment = parent_comment
+            reply.save()
+            return redirect('blog_detail',pk=pid)
+
+    context = {'parent_comment': parent_comment,'reply_form':reply_form, 'blog':blog}
+    return render(request, 'blog_app/add_reply.html',context=context)
